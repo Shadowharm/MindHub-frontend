a3a2105e1eb5a049ff87589485cce2c00128ee4a
maksim.prokhorov <pmv.maks@gmail.com>
2024-10-15 20:05:07 +0300

    feat: initialize frontend

---
+++ src/app/providers.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { QueryClient } from '@tanstack/query-core'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import React, { PropsWithChildren, useState } from 'react'
+
+export function Providers({ children }: PropsWithChildren) {
+	const [client] = useState(
+		new QueryClient({
+			defaultOptions: {
+				queries: {
+					refetchOnWindowFocus: false
+				}
+			}
+		})
+	)
+
+	return (
+		<QueryClientProvider client={client}>
+			{children}
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	)
+}
